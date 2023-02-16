6ec1f731e635775ea23895b88bcf525e9eac37aa
AbrahamJ29 <abrahamappels@gmail.com>
2023-02-16 12:23:08 -0800

    6-size.c

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/** main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a newline, to the standard error.
+ * Return: Always 1.
+ */
+
+int main(void)
+{
+	write(2, änd that piece of art is useful\ -Dora kopar, 2015-10-19\n", 5#);
+	return (1);
+}
