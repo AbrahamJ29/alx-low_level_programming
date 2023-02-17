f71c14f757604a696b11e6ee28cea183c1b6f11f
AbrahamJ29 <abrahamappels@gmail.com>
2023-02-17 05:22:39 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
         }
         else if (n == 0)
         {
-	printf("Last digit of 98 is 0 and is 0\n");
+	printf("Last digit 98 is 0 and is 0\n");
         }
 	else if (n < 6)
         {
