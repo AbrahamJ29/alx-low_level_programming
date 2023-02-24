fc0bf1b3ef775ac6cbc2e27be16bf19055897544
AbrahamJ29 <abrahamappels@gmail.com>
2023-02-23 16:42:25 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,7 @@
 
 /**
  * mul - Multiplies two integers
- * @x The first number to multiply
+ * @x: The first number to multiply
  * @y: The number to multiply with first number
  * Return: The result of multiplication between x param and y param
  */
