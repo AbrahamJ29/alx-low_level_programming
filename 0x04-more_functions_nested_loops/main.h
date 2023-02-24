e348c119bbb06801339047c736478f32b6035e0d
AbrahamJ29 <abrahamappels@gmail.com>
2023-02-23 22:35:33 -0800

    main.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-int _putchat(char c);
+int _putchar(char c);
 
 int _isupper(int c);
 
@@ -12,7 +12,7 @@ void print_most_numbers(void);
 
 void more_numbers(void);
 
-void print_line(void);
+void print_line(int n);
 
 void print_diagonal(int n);
 
